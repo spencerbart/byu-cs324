a52cd6760978a470e1bb3f49777eca20e09c2c8c
konga400 <sp3nc3rbart@gmail.com>
2022-09-01 20:41:50 -0600

    working build

--- hw-catmatch/catmatch.c
+++ hw-catmatch/catmatch.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#define BUFSIZE 80
+#define MAX_LINE_LENGTH 1000
 
 void main(int argc, char *argv[])
 {
@@ -18,6 +21,24 @@ void main(int argc, char *argv[])
     //     printf("One argument expected.\n");
     // }
 
+    // grab env
+    char pattern[BUFSIZE];
+    char *envvar = "CATMATCH_PATTERN";
+
+    // Make sure envar actually exists
+    if(!getenv(envvar)){
+        fprintf(stderr, "The environment variable %s was not found.\n", envvar);
+        exit(1);
+    }
+
+    // Make sure the buffer is large enough to hold the environment variable
+    // value. 
+    if(snprintf(pattern, BUFSIZE, "%s", getenv(envvar)) >= BUFSIZE){
+        fprintf(stderr, "BUFSIZE of %d was too small. Aborting\n", BUFSIZE);
+        exit(1);
+    }
+    // printf("pattern: %s\n", pattern);
+
     // takes a file name and creates a file pointer.
     FILE *file_pointer;
 
@@ -29,11 +50,29 @@ void main(int argc, char *argv[])
     }
 
     // Read contents from file
-    char c = fgetc(file_pointer);
-    while (c != EOF)
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, MAX_LINE_LENGTH, file_pointer))
     {
-        printf ("%c", c);
-        c = fgetc(file_pointer);
+        int count = 0;
+        // loop through line and count how many times it matches pattern
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            int char_count = 0;
+            for (size_t j = 0; j < strlen(pattern); j++)
+            {
+                if (line[i+j] == pattern[j])
+                {
+                    char_count++;
+                }
+            }
+            if (char_count == strlen(pattern))
+            {
+                count++;
+            }
+            
+        }
+
+        printf("%d %s", count, line);
     }
 
     fclose(file_pointer);
