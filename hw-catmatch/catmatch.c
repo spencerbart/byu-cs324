4a5552956994ffb11cc89bd56978e3f609ee7c76
konga400 <sp3nc3rbart@gmail.com>
2022-09-02 18:22:45 -0600

    finished

--- hw-catmatch/catmatch.c
+++ hw-catmatch/catmatch.c
@@ -1,3 +1,64 @@
+//  1. What are the numbers associated with the manual sections for executable
+//     programs, system calls, and library calls, respectively?
+    
+//     Executable programs or `1`
+    
+//     System calls `2`
+    
+//     Library calls `3`
+
+//  2. Which section number(s) contain a man page for `kill`?
+
+//     `12`
+
+//  3. Which section number(s) contain a man page for `exit`?
+
+//     `3`
+
+//     `2`
+
+//  4. Which section number(s) contain a man page for `open`?
+
+//     `2`
+
+//  5. What three `#include` lines should be included to use the `open()` system
+//     call?
+
+//     ```C
+//        #include <sys/types.h>
+//        #include <sys/stat.h>
+//        #include <fcntl.h>
+//     ```
+//  6. Which section number(s) contain a man page for `socket`?
+
+//     `7`
+
+//     `2`
+
+//  7. Which `socket` option "Returns a value indicating whether or not this
+//     socket has been marked to accept connections with `listen(2)`"?
+
+//     `SO_ACCEPTCONN`
+
+//  8. How many man pages (in any section) contain keyword references to
+//     `getaddrinfo`?
+
+//     `3`
+
+//  9. According to the "DESCRIPTION" section of the man page for `string`, the
+//     functions described in that man page are used to perform operations on
+//     strings that are ________________. (fill in the blank)
+
+//     `null-terminated strings`
+
+//  10. What is the return value of `strcmp()` if the value of its first argument
+//      (i.e., `s1`) is _greater than_ the value of its second argument (i.e.,
+//      `s2`)?
+
+//      `>0`
+
+// I completed the TMUX exercise from Part 2
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,24 +72,25 @@ void main(int argc, char *argv[])
     __pid_t process_id = getpid();
     fprintf(stderr, "%d\n\n", process_id);
 
-    // if( argc == 2 ) {
-    //     printf("The argument supplied is %s\n", argv[1]);
-    // }
-    // else if( argc > 2 ) {
-    //     printf("Too many arguments supplied.\n");
-    // }
-    // else {
-    //     printf("One argument expected.\n");
-    // }
+    if( argc == 2 ) {
+    }
+    else if( argc > 2 ) {
+        printf("Too many arguments supplied.\n");
+    }
+    else {
+        printf("One argument expected.\n");
+    }
 
     // grab env
     char pattern[BUFSIZE];
     char *envvar = "CATMATCH_PATTERN";
+    int envIsPresent = 1;
 
     // Make sure envar actually exists
     if(!getenv(envvar)){
-        fprintf(stderr, "The environment variable %s was not found.\n", envvar);
-        exit(1);
+        // fprintf(stderr, "The environment variable %s was not found.\n", envvar);
+        // exit(1);
+        envIsPresent = 0;
     }
 
     // Make sure the buffer is large enough to hold the environment variable
@@ -70,6 +132,14 @@ void main(int argc, char *argv[])
                 count++;
             }
             
+        }
+
+        if (count > 0) 
+        {
+            count = 1;
+        }
+        if (!envIsPresent) {
+            count = 0;
         }
 
         printf("%d %s", count, line);
